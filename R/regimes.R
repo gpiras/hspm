be80f549fcc5df98725224c3f4aa1985f7568cba
gpiras <gpiras@mac.com>
2022-02-23 17:21:26 +0100

    preliminary version with non-spatial regimes only

--- R/regimes.R
+++ R/regimes.R
@@ -6,12 +6,8 @@
 #' @name regimes
 #' @param formula a symbolic description of the model.
 #' @param data the data of class \code{data.frame}.
-#' @param splitv variable to split the dataset
-#' @param vc   one of ("homoskedastic", "groupwise","heteroskedastic")
-#' @param x object for printing
-#' @param digits number of digits
-#' @param object object for printing
-#' @param ... additional arguments to be passed
+#' @param rgv variable to identify the regimes
+#' @param vc   one of ("homoskedastic", "groupwise")
 #'
 #' @details
 #'
@@ -25,94 +21,138 @@
 #'
 #'
 #' @author Gianfranco Piras and Mauricio Sarrias
-#' @return An object of class ``\code{spregimes}'', a list with elements:
-#' \item{coefficients}{the estimated coefficients,}
-#' \item{call}{the matched call,}
-#' \item{X}{the X matrix}
-#' \item{y}{the dependent variable}
-#' #' @import Matrix stats spatialreg methods Formula maxLik sphet
+#' @return An object of class ``\code{lm}'', a list with elements:
+#' @import Formula sphet stats
 #' @export
 
-regimes <- function(formula, data, splitv,
-                    vc = c("homoskedastic", "groupwise","heteroskedastic")){
+regimes <- function(formula, data, rgv = NULL,
+                    vc = c("homoskedastic", "groupwise")){
+
+
+
+  if(is.null(rgv)) stop("regimes variable not specified")
+  if(class(rgv) != "formula") stop("regimes variable has to be a formula")
 
   #Obtain arguments
-  vc <- match.arg(vc)
-  splitvar <- as.matrix(lm(splitv, data, method="model.frame"))
-  sv <- length(unique(splitvar))
+  vc                <- match.arg(vc)
 
+  #process the data
+  intro    <- data.prep.regimes(formula = formula, data = data, rgv = rgv)
 
-  if(vc == "groupwise") res <- groupwise.regimes(formula, data, splitvar)
+  #split the object list intro:
+  # data
+  # formula
+  # k tot number of variables in model
+  # totr tot of variables varying by regime
+  dataset  <- intro[[1]]
+  form     <- intro[[2]]
+  k        <- intro[[3]]
+  k1       <- intro[[4]]
+  k2       <- intro[[5]]
+  rgm      <- intro[[6]]
+  sv       <- intro[[7]]
 
-  else{
-    ##do it as a function to accomodate all types of dataset
-    nobsg <- table(splitvar)
-    csnobsg <- cumsum(nobsg)
-    cnt <-  c(1,table(splitvar)+1)[-(sv+1)]
-    mt <- terms(formula, data = data)
-    mf <- model.frame(formula, data = data)
-    data <- data[order(splitvar),]
-    y <- model.response(mf)
-    x <- model.matrix(mt, mf)
-    n <- dim(x)[1]
-    k <- dim(x)[2]
-    namesx <- colnames(x)
-    namesxr <- paste(namesx,rep(1:sv,each = k), sep = "_")
-    totr <- sv*k
-    xr <- matrix(0, ncol = totr, nrow = n)
-    seq_1 <- seq(1, totr, k)
-    seq_2 <- seq(k, totr,  k)
-    for(i in 1:length(nobsg)) {
-      xr[(cnt[i]:csnobsg[i]),seq_1[i]:seq_2[i]] <- x[(cnt[i]):csnobsg[i], ]
-    }
-    #xr <- as.data.frame(xr)
-    names(xr) <- namesxr
+if (vc == "groupwise")  res <- groupwise.regimes(form, dataset, k, k1, k2, rgm, sv)
 
-  if(vc == "homoskedastic") res <- lm(y~ xr-1, data = data)
-  if(vc == "heteroskedastic") stop("hetero not yet implemented")
-  }
-  class(res) <- "spregimes"
+if (vc == "homoskedastic")  res <- lm(form, dataset)
+
+  #class(res) <- "spregimes"
 return(res)
   }
 
 
-groupwise.regimes <- function(formula, data, splitvar){
+groupwise.regimes <- function(formula, data, k, k1, k2, rgm, sv){
 
-  dataset <- split.data.frame(data, splitvar)
-  res <- lapply(dataset, lm, formula = formula)
+  if (is.null(k2)) df <- k
+  else df <- k1 + 2*k2
+
+
+  fs <- lm(formula, data)
+  uhat <- residuals(fs)
+  nobsg <- colSums(rgm)
+
+  omega <- vector("numeric", length = nrow(data))
+  for (i in 1: sv) omega[which(as.numeric(rgm[,i])==1)] <-  1/(crossprod(residuals(fs)[which(as.numeric(rgm[,i])==1)])/(nobsg[i]-df))
+
+  data$omega <- omega
+  res <-  lm(formula, data, weights = omega)
+
   return(res)
 }
 
-#' @rdname regimes
-#' @method print spregimes
-#' @import stats
-#' @export
-print.spregimes <- function(x,
-                         digits = max(3, getOption("digits") - 3),
-                         ...)
-{
-  cat("Call:\n")
-  print(x$call)
-  cat("\nCoefficients:\n")
-  print.default(format(drop(coef(x)), digits = digits), print.gap = 2,
-                quote = FALSE)
-  cat("\n")
-  invisible(x)
+homoskedastic.regimes <- function(formula, dataset){
+  res <- lm(formula, dataset)
+  return(res)
 }
+data.prep.regimes <- function(formula, data, rgv){
 
-#' @rdname regimes
-#' @method summary spregimes
-#' @import stats
-#' @export
-summary.spregimes <- function(object, ...){
-  b                   <- object$coefficients
-  std.err             <- sqrt(diag(vcov(object)))
-  z                   <- b / std.err
-  p                   <- 2 * (1 - pnorm(abs(z)))
-  CoefTable           <- cbind(b, std.err, z, p)
-  colnames(CoefTable) <- c("Estimate", "Std. Error", "z-value", "Pr(>|z|)")
-  object$CoefTable    <- CoefTable
-  class(object)       <- c("summary.spregime", "spregime")
-  return(object)
+  splitvar         <- as.matrix(lm(rgv, data, method="model.frame"))
+  sv               <- length(unique(splitvar))
+  svm              <- as.numeric(unique(splitvar))
+  rgm              <-  matrix(,nrow = nrow(data), ncol = 0)
+  for(i in svm)    rgm <- cbind(rgm, ifelse(splitvar ==  i, 1, 0))
+
+
+
+  f1 <- Formula(formula)
+  mt <- terms(f1, data = data)
+  mf1 <- model.frame(f1, data = data)
+  y <- model.response(mf1)
+
+  k1 <- NULL
+  k2 <- NULL
+
+  if(length(f1)[2L] == 2L){
+    x1 <- model.matrix(f1, data = mf1, rhs = 1)
+    namesx1 <- colnames(x1)
+    x2 <- model.matrix(f1, data = mf1, rhs = 2)
+    namesx <- colnames(x2)
+    if(any(namesx1 == "(Intercept)") && any(colnames(x2) == "(Intercept)"))
+                stop("(Intercept) cannot  be specified in both formula")
+    n <- dim(x1)[1]
+    k1 <- dim(x1)[2]
+    k2 <- dim(x2)[2]
+    if(any(namesx1 == "(Intercept)")) namesx1[which(namesx1 == "(Intercept)")]  = "Intercept"
+    if(any(namesx == "(Intercept)")) namesx[which(namesx == "(Intercept)")] = "Intercept"
+    namesxr <- paste(namesx,rep(1:sv,each = k2), sep = "_")
+
+    totc <- sv*k2
+    k <- k1 + totc
+
+    xr <- matrix(0, ncol = totc, nrow = n)
+    seq_1 <- seq(1, totc, k2)
+    seq_2 <- seq(k2, totc,  k2)
+    for(i in 1:sv) xr[,seq_1[i]:seq_2[i]] <- x2 * rgm[,i]
+
+    data <- data.frame(y, x1,xr)
+    colnames(data) <- c("y", namesx1, namesxr)
+    form <- as.formula(paste("y ~", paste(names(data)[2:(k+1)], collapse =  " + "), "-1"))
+
+  }
+  else{
+    x <- model.matrix(f1, data = mf1, rhs = 1)
+    n <- dim(x)[1]
+    k <- dim(x)[2]
+    namesx <- colnames(x)
+    if(any(namesx == "(Intercept)")) namesx[ which(namesx == "(Intercept)")] = "Intercept"
+    namesxr <- paste(namesx,rep(1:sv,each = k), sep = "_")
+    totc <- sv*k
+
+    xr <- matrix(0, ncol = totc, nrow = n)
+    seq_1 <- seq(1, totc, k)
+    seq_2 <- seq(k, totc,  k)
+
+    for(i in 1:sv)     xr[,seq_1[i]:seq_2[i]] <- x * rgm[,i]
+    data <- data.frame(y,xr)
+
+    colnames(data) <- c("y" ,namesxr)
+
+    if("Intercept_1" %in% namesxr)  form <- as.formula(paste("y ~", paste(names(data)[2:(totc+1)], collapse =  " + "), "-1"))
+    else form <- as.formula(paste("y ~", paste(names(data)[2:(totc+1)], collapse =  " + ")))
+
+  }
+
+
+  ret <- list(data, form, k, k1, k2, rgm, sv)
+return(ret)
 }
-
